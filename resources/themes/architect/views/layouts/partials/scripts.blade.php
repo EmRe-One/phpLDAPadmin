2ddc0618fbedf32baf4e5f2916ed91ca652f14ea
Emre Ak <emre.akguel.1@gmail.com>
2023-03-23 23:14:45 +0100

    add missing jquery

--- resources/themes/architect/views/layouts/partials/scripts.blade.php
+++ resources/themes/architect/views/layouts/partials/scripts.blade.php
@@ -1,3 +1,4 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 <script src="{{ asset('/js/app.js') }}" type="text/javascript"></script>
 <!-- bootstrap -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
